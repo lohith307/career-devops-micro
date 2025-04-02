39a474de733d7fc18e114db1388e860ba064e26b
lohith307 <85469965+lohith307@users.noreply.github.com>
2025-04-02 03:25:46 +0000

    key name was updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance-1" {
     ami = "ami-0453ec754f44f9a4a"
     instance_type = "t2.micro"
     security_groups = ["default"]
-    key_name = "lano"
+    key_name = "lani"
     user_data = file("grafana.sh")
     tags = {
         Name = "Grafana-Server"
@@ -17,7 +17,7 @@ resource "aws_instance" "instance-2" {
     ami = "ami-0453ec754f44f9a4a"
     instance_type = "t2.micro"
     security_groups = ["default"]
-    key_name = "lano"
+    key_name = "lani"
     tags = {
         Name = "Node-exporter"
     }
