475cb373fc68232d8f1d87b692e1035232043b70
lohith307 <85469965+lohith307@users.noreply.github.com>
2025-04-02 03:13:40 +0000

    Updated Terraform configuration for EC2 instances and security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance-1" {
     ami = "ami-0453ec754f44f9a4a"
     instance_type = "t2.micro"
     security_groups = ["default"]
-    key_name = "project"
+    key_name = "lano"
     user_data = file("grafana.sh")
     tags = {
         Name = "Grafana-Server"
@@ -17,7 +17,7 @@ resource "aws_instance" "instance-2" {
     ami = "ami-0453ec754f44f9a4a"
     instance_type = "t2.micro"
     security_groups = ["default"]
-    key_name = "project"
+    key_name = "lano"
     tags = {
         Name = "Node-exporter"
     }
